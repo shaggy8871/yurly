4dd07cf11278a7a397e3f6d494a6699aab50fae5
Tudor Popescu <Donkfather@users.noreply.github.com>
2020-02-20 13:44:20 +0200

    Add test for multiple flashes

--- tests/Tests/ResponseTest.php
+++ tests/Tests/ResponseTest.php
@@ -3,9 +3,23 @@
 namespace Tests;
 
 use PHPUnit\Framework\TestCase;
+use Yurly\Request\Request;
 
 class ResponseTest extends TestCase
 {
+    public function testFlashWithMultipleKeys()
+    {
+        $project = new \Yurly\Core\Project(__NAMESPACE__, 'tests', true);
+        $ctx = new \Yurly\Core\Context($project);
+        $response = new \Yurly\Response\Response($ctx);
+
+        $response->flash('first_key', 'first_value');
+        $response->flash('second_key', 'second_value');
+
+        $request = new Request($ctx);
+        $this->assertEquals('first_value', $request->getFlash('first_key'));
+        $this->assertEquals('second_value', $request->getFlash('second_key'));
+    }
 
     public function testResponseInterface()
     {
